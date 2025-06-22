480957906a6d53257b99a5a0c2a241419f4a1411
Dalton <daltonadams111@gmail.com>
2025-06-21 22:24:04 -0500

    added diamond sounds
    created and added collection and collision sounds for diamonds.
    implimented the sound affects for diamonds.

--- scenes/Collectables/Diamond/collectable_diamond.gd
+++ scenes/Collectables/Diamond/collectable_diamond.gd
@@ -2,6 +2,17 @@ extends Collectable
 class_name CollectableDiamond
 
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var collision_sound: AudioStreamPlayer2D = $CollisionSound
+@onready var collection_sound: AudioStreamPlayer2D = $CollectionSound
+
+
 func _on_collected(_body: Node2D, inventory: InventoryComponent) -> void:
 	inventory.add_item(item, 1)
-	queue_free()
+	collection_sound.pitch_scale = randf_range(0.95, 1.05)
+	animation_player.play("collect")
+
+
+func _on_body_entered(_body: Node) -> void:
+	collision_sound.pitch_scale = randf_range(0.95, 1.05)
+	collision_sound.play()
